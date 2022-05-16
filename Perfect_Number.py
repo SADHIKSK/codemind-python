447c5e764318fc0e821d15e963851c8e24dfd9cf
Saadhik shaik <96807695+SADHIKSK@users.noreply.github.com>
2022-05-16 18:26:25 +0530

    n=int(input()) s=0 x=1 while x<=n//2:     if n%x==0:         s+=x     x+=1 if (s==n):     print("True") else:     print('False')

--- Perfect_Number.py
+++ Perfect_Number.py
@@ -2,11 +2,10 @@ n=int(input())
 s=0
 x=1
 while x<=n//2:
-    if(n%x==0):
+    if n%x==0:
         s+=x
     x+=1
-if(s==n):
-    print('True')
+if (s==n):
+    print("True")
 else:
     print('False')
-    
